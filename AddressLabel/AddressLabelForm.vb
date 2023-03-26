3c4949baedce7274ce023e3cbd7b327259a4bfaf
Chris Pickens <Cpickens1270@gmail.com>
2023-03-26 05:43:52 -0600

    Code clear and exit buttons

--- AddressLabel/AddressLabelForm.vb
+++ AddressLabel/AddressLabelForm.vb
@@ -8,5 +8,27 @@ Option Explicit On
 Option Strict On
 
 Public Class AddressLabelForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        FirstNameTextBox.Clear()
+        LastNameTextBox.Clear()
+        StreetAddressTextBox.Clear()
+        CityTextBox.Clear()
+        StateTextBox.Clear()
+        ZipTextBox.Clear()
+
+        FirstNameErrorLabel.Visible = False
+        LastNameErrorLabel.Visible = False
+        StreetAddressErrorLabel.Visible = False
+        CityErrorLabel.Visible = False
+        StateErrorLabel.Visible = False
+        ZipErrorLabel.Visible = False
+
+        DisplayLabel.Text = ""
+
+        FirstNameTextBox.Select()
+    End Sub
 End Class
